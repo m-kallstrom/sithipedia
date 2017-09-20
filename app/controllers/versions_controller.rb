177b91215ac551c44695a888d062adbfcbabfc7a
Gwynne Noth <nothgwynne@gmail.com>
2017-09-20 16:22:33 -0500

    Write controller methods for new versions and articles

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -4,12 +4,36 @@ class VersionsController < ApplicationController
     @version = Version.new
   end
 
+  # this route in some respects is the articles/edit route
   def create
-    @version = Version.create(version_params)
+    @version = Version.new(version_params)
+    errors_page = page_to_render_if_errors(@version)
+
+    @article = Article.find_or_create_by(version_params[:article_id])
+
+    if @version.save
+      redirect_to '/articles'
+    else
+      @errors = @version.errors.full_messages
+      render errors_page #may need to change this
+    end
   end
 
   private
   def version_params
-
+    params.require(:version).permit(:title, :body, :published, :category_id, :article_id)
   end
+
+  def brand_new_article?(version)
+    version.article_id == nil
+  end
+
+  def page_to_render_if_errors(version)
+    if brand_new_article?(version)
+      '/articles/new'
+    else
+      '/articles/edit'
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,5 @@ Rails.application.routes.draw do
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
 
-
+  get '/articles/new' => 'versions#new'
 end
